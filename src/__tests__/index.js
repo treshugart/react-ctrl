16832336d5bfcd13a5fb0ebed1421c589234bba7
Trey Shugart <treshugart@gmail.com>
2018-01-31 17:25:35 +1100

    Fix flow errors.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,13 +1,17 @@
+// @flow
+
 import { mount } from "enzyme";
 import React, { Component, Fragment } from "react";
 import withCtrl from "..";
 
+const { expect, test } = global;
+
 const Comp = withCtrl(
-  class extends Component {
+  class extends Component<any, { prop: string }> {
     constructor(props) {
       super(props);
       this.state = {
-        prop: props.prop
+        prop: props.prop || ""
       };
     }
     render() {
